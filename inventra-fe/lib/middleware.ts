7344f8741d695fe94fc8c097e0923b68897ddd7a
muhammadarkanmariadi-debug <muhammadarkanmariadi@gmail.com>
2026-04-24 15:30:32 +0700

    docker

--- inventra-fe/lib/middleware.ts
+++ inventra-fe/lib/middleware.ts
@@ -6,8 +6,8 @@ export function middleware(request: NextRequest) {
   const token = request.cookies.get('token');
 
 
-  if (!token && !request.nextUrl.pathname.startsWith('/login')) {
-    return NextResponse.redirect(new URL('/login', request.url));
+  if (!token && !request.nextUrl.pathname.startsWith('/signin')) {
+    return NextResponse.redirect(new URL('/signin', request.url));
   }
 
   return NextResponse.next();
